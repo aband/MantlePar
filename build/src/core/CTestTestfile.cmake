92ddf0160edbc325781f84872c63a7a1321d165e
aband <abandtian@gmail.com>
2026-09-13 11:31:41 +0200

    remove build

--- build/src/core/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/renpo/Research/MantlePar/src/core
-# Build directory: /home/renpo/Research/MantlePar/build/src/core
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
